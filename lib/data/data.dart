15c9a3d0bd7a5a21d1fcfd4f2a3a397287221069
P-Jag <pavel.jagla@gmail.com>
2020-09-08 22:24:08 +0200

    initial commit - basic set up

---
+++ lib/data/data.dart
@@ -0,0 +1,10 @@
+import 'package:food_delivery_app/models/food.dart';
+import 'package:food_delivery_app/models/order.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
+import 'package:food_delivery_app/models/user.dart';
+
+//Food
+
+//Restaurant
+
+//User
