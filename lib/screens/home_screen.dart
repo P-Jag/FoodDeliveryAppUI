14429937c9d299bed76f2b91f1942db8b6033694
P-Jag <pavel.jagla@gmail.com>
2020-09-09 09:10:27 +0200

    data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/data/data.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        title: Text('Food Delivery'),
+        actions: [
+          FlatButton(
+            child: Text('Cart (${currentUser.cart.length})'),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
   }
 }
