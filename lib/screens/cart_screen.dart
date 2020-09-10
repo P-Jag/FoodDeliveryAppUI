c1ed6be5a888540ff94aff5353716d805ba9de9b
P-Jag <pavel.jagla@gmail.com>
2020-09-10 13:05:27 +0200

    complete UI

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,6 +3,10 @@ import 'package:food_delivery_app/data/constants.dart';
 import 'package:food_delivery_app/data/data.dart';
 import 'package:food_delivery_app/models/order.dart';
 
+import '../data/constants.dart';
+import '../data/constants.dart';
+import '../data/constants.dart';
+
 class CartScreen extends StatefulWidget {
   @override
   _CartScreenState createState() => _CartScreenState();
@@ -103,6 +107,10 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach(
+        (Order order) => totalPrice += order.quantity * order.food.price);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -110,17 +118,97 @@ class _CartScreenState extends State<CartScreen> {
         ),
       ),
       body: ListView.separated(
-          itemBuilder: (BuildContext context, int index) {
+        itemBuilder: (BuildContext context, int index) {
+          if (index < currentUser.cart.length) {
             Order order = currentUser.orders[index];
             return _buildCartItem(order);
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider(
-              height: 1.0,
-              color: Colors.grey,
-            );
-          },
-          itemCount: currentUser.cart.length),
+          }
+          return Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Estimated Delivery Time:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '25 minutes',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Final cost of your meal:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '\$${totalPrice.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 65.0),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(
+            height: 1.0,
+            color: Colors.grey,
+          );
+        },
+        itemCount: currentUser.cart.length + 1,
+      ),
+      bottomSheet: Container(
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: 100.0,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, -1),
+              blurRadius: 8.0,
+            ),
+          ],
+        ),
+        child: Center(
+          child: FlatButton(
+            child: Text(
+              'CHECKOUT',
+              style: TextStyle(
+                color: Colors.grey[100],
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
   }
 }
